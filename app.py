2ea63716656520807bdd6eba9148358d75546236
ye zhiming <mail@54zm.com>
2021-12-07 14:16:01 +0800

    初始化项目

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+import collection
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def index():
+    page = request.args.get("page")
+    if page is None:
+        return {"code": 500, "msg": "请传递 page 页数"}
+    else:
+        page = "1"
+    ok = collection.Start(page)
+    if ok:
+        return {"code": 200, "msg": "ok", "data": json.loads(ok)}
+    else:
+        return {"code": 500, "msg": "redis缓存失败"}
